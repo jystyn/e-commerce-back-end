24b0aacb214338fea7daa6159ddcce7eef2efbec
joostyn <justyn.helgeson@gmail.com>
2022-08-03 18:46:03 -0500

    Wrote and successfully tested find all categories and find one category by its id value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,20 +5,21 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
-  // Category.findAll(({
-  //   include: Product
-  // }))
-  //   .then(category_data => res.json(category_data))
-  //   .catch(err => {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   });
+  Category.findAll({
+    include: Product
+  })
+    .then(category_data => res.json(category_data));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: Product
+  })
+    .then(category_data => res.json(category_data));
 });
 
 router.post('/', (req, res) => {
