579bdb8aedc6991191f5560d296da5b6a63578e6
joostyn <justyn.helgeson@gmail.com>
2022-08-03 17:55:43 -0500

    Fixed Models

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,12 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
@@ -20,6 +26,13 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
