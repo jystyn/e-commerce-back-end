76251543ea764b6fcab6a161b5ea08845781c924
joostyn <justyn.helgeson@gmail.com>
2022-08-03 17:34:33 -0500

    Required sequelize. wrote sync in server.js. Started in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,14 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  // Category.findAll(({
+  //   include: Product
+  // }))
+  //   .then(category_data => res.json(category_data))
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
 });
 
 router.get('/:id', (req, res) => {
