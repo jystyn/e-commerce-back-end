fef43bcad174d616fed259b912029d8fe46eaa71
joostyn <justyn.helgeson@gmail.com>
2022-08-03 22:39:15 -0500

    Wrote tag-routes and tested through insomnia

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,52 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: Product
+  })
+    .then(tag_data => res.json(tag_data));
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: Product
+  })
+    .then(data_data => res.json(data_data));
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(tag_data => res.json(tag_data));
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(tag_data => res.json(tag_data));
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tag_data => res.json(tag_data));
 });
 
 module.exports = router;
